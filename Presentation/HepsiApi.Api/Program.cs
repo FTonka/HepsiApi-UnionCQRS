67012cde7641a22b641e1d862c1658bc32b9f3a4
Fettah Tonka <Fettah.tonka@gmail.com>
2024-10-17 14:43:35 +0300

    Creating db and transfering entities into it

--- Presentation/HepsiApi.Api/Program.cs
+++ Presentation/HepsiApi.Api/Program.cs
@@ -1,3 +1,4 @@
+using HepsiApi.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,7 +12,9 @@ var env = builder.Environment;
 builder.Configuration
     .SetBasePath(env.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false)
-    .AddJsonFile($"appsettings.json{env.EnvironmentName}", optional: true);
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+builder.Services.AddPersistence(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
